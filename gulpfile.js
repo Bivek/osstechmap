587d89e496ce7a16eb13e09d3ca63da14c887046
Markus Klems <markusklems@gmail.com>
2016-06-16 11:01:04 +0200

    Setting up gulp build process.
    Thanks for the perfect sample by
    https://markgoodyear.com/2014/01/getting-started-with-gulp/

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', ['clean'], function() {
+    gulp.start('scripts');
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('app/js/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(notify({
+            message: 'Scripts task complete'
+        }));
+});
+
+gulp.task('clean', function() {
+    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
