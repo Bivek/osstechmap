5fd190abd53524d1f2218c21dfba23d50249d9ee
Markus Klems <markusklems@gmail.com>
2016-06-16 18:29:53 +0200

    Trying out groups
    We could use groups to label technologies with certain concepts or
    properties. However, it does not seem to be possible to tag a node with
    multiple group tags. Maybe rather build an own tagging mechanism.

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,24 @@
 var nodes;
 var edges;
+var options;
 
 // Get "Microservices" related OSS
 var getNodesP = $.getJSON("data/ms.nodes.json");
 var getEdgesP = $.getJSON("data/ms.edges.json");
+var getOptionsP = $.getJSON("data/ms.options.json");
 
-$.when(getNodesP, getEdgesP).done(function(nodesData, edgesData) {
+$.when(getNodesP, getEdgesP, getOptionsP).done(function(nodesData, edgesData, optionsData) {
     console.log(JSON.stringify(nodesData));
     console.log(JSON.stringify(edgesData));
+    console.log(JSON.stringify(optionsData));
     // create an array with nodes
     nodes = new vis.DataSet(nodesData[0]);
     // create an array with edges
     edges = new vis.DataSet(edgesData[0]);
+    // create an options object with concepts (groups)
+    options = new vis.DataSet(optionsData[0])._options;
+
+    console.log("options => " + JSON.stringify(options));
 
     // create a network
     var container = document.getElementById('oss-network');
@@ -19,6 +26,5 @@ $.when(getNodesP, getEdgesP).done(function(nodesData, edgesData) {
         nodes: nodes,
         edges: edges
     };
-    var options = {};
     var network = new vis.Network(container, data, options);
 });
