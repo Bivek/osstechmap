5da81d4704823b87bd79c45d8ade2abcf6efc047
Markus Klems <markusklems@gmail.com>
2016-06-17 15:47:11 +0200

    Trying out d3
    Examples by http://bl.ocks.org/mbostock/4062045

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,76 @@
-var nodes;
-var edges;
-var options;
+var width = 960,
+    height = 500;
 
-// Get "Microservices" related OSS
-var getNodesP = $.getJSON("data/ms.nodes.json");
-var getEdgesP = $.getJSON("data/ms.edges.json");
-var getOptionsP = $.getJSON("data/ms.options.json");
+var color = d3.scale.category20();
 
-$.when(getNodesP, getEdgesP, getOptionsP).done(function(nodesData, edgesData, optionsData) {
-    console.log(JSON.stringify(nodesData));
-    console.log(JSON.stringify(edgesData));
-    console.log(JSON.stringify(optionsData));
-    // create an array with nodes
-    nodes = new vis.DataSet(nodesData[0]);
-    // create an array with edges
-    edges = new vis.DataSet(edgesData[0]);
-    // create an options object with concepts (groups)
-    options = new vis.DataSet(optionsData[0])._options;
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(30)
+    .size([width, height]);
 
-    console.log("options => " + JSON.stringify(options));
+var svg = d3.select("#techmap").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-    // create a network
-    var container = document.getElementById('oss-network');
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    var network = new vis.Network(container, data, options);
+d3.json("data/microservices.json", function(error, graph) {
+    if (error) throw error;
+
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) {
+            return Math.sqrt(d.value);
+        });
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", function(d) {
+            return color(d.group);
+        })
+        .call(force.drag);
+
+    node.append("title")
+        .text(function(d) {
+            return d.name;
+        });
+
+    node.append("text")
+        .text(function(d) {
+            return d.name;
+        });
+
+    node.attr("tags", function(d) {
+        return d.tags;
+    });
+
+    force.on("tick", function() {
+        link.attr("x1", function(d) {
+                return d.source.x;
+            })
+            .attr("y1", function(d) {
+                return d.source.y;
+            })
+            .attr("x2", function(d) {
+                return d.target.x;
+            })
+            .attr("y2", function(d) {
+                return d.target.y;
+            });
+
+        node.attr("cx", function(d) {
+                return d.x;
+            })
+            .attr("cy", function(d) {
+                return d.y;
+            });
+    });
 });
